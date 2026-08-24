4ceccf341876f89a59933a61795da6297dd5ff8f
Fengyuan Liu <fengyl8@uci.edu>
2026-08-23 18:10:16 -0700

    feat: create and list expenses

---
+++ src/main/java/com/fengyuan/expense_tracker/controller/ExpenseController.java
@@ -0,0 +1,32 @@
+package com.fengyuan.expense_tracker.controller;
+
+import com.fengyuan.expense_tracker.model.Expense;
+import com.fengyuan.expense_tracker.service.ExpenseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @PostMapping
+    public Expense createExpense(@RequestBody Expense expense) {
+        return expenseService.createExpense(expense);
+    }
+
+    @GetMapping
+    public List<Expense> getAllExpenses() {
+        return expenseService.getAllExpenses();
+    }
+}
